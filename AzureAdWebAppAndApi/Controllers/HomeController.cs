ba963679180ce3c384567e9e90dc3540bba8c2eb
snanditech <snandi.tech@gmail.com>
2023-02-06 19:31:38 +0530

    Add project files.

---
+++ AzureAdWebAppAndApi/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using AzureAdWebAppAndApi.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Web;
+using System.Diagnostics;
+
+namespace AzureAdWebAppAndApi.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private ITokenAcquisition _tokenAcquisition;
+
+        public HomeController(ILogger<HomeController> logger,ITokenAcquisition tokenAcquisition)
+        {
+            _logger = logger;
+            _tokenAcquisition = tokenAcquisition;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Privacy()
+        {
+            var scopes = new List<string>() { "8b953e05-fc1a-48c7-b5c9-d17cabbfdc6f/.default" };
+            var test = await _tokenAcquisition.GetAccessTokenForUserAsync(scopes);
+            return View();
+        }
+
+        [AllowAnonymous]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
